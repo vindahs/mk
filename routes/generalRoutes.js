4c82cfeaf8c18506471a796e535db2ca4e125b01
vindahs <vincentonoja@ymail.com>
2023-10-05 12:37:44 +0100

    first commit

---
+++ routes/generalRoutes.js
@@ -0,0 +1,201 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+    res.render('contact', { title: 'Express' });
+});
+
+router.get('/properties', function(req, res, next) {
+    res.render('properties', { title: 'Express' });
+});
+
+router.get('/aminu', function(req, res, next) {
+    res.render('aminu', { title: 'Express' });
+});
+
+router.get('/onoja', function(req, res, next) {
+    res.render('onoja', { title: 'Express' });
+});
+
+router.get('/beshira', function(req, res, next) {
+    res.render('beshira', { title: 'Express' });
+});
+
+router.get('/chinaza', function(req, res, next) {
+    res.render('chinaza', { title: 'Express' });
+});
+
+router.get('/favor', function(req, res, next) {
+    res.render('favor', { title: 'Express' });
+});
+
+router.get('/ij', function(req, res, next) {
+    res.render('ij', { title: 'Express' });
+});
+
+router.get('/jamila', function(req, res, next) {
+    res.render('jamila', { title: 'Express' });
+});
+
+router.get('/ladi', function(req, res, next) {
+    res.render('ladi', { title: 'Express' });
+});
+
+router.get('/martha', function(req, res, next) {
+    res.render('martha', { title: 'Express' });
+});
+
+router.get('/mathias', function(req, res, next) {
+    res.render('mathias', { title: 'Express' });
+});
+
+router.get('/monday', function(req, res, next) {
+    res.render('monday', { title: 'Express' });
+});
+
+router.get('/rejoice', function(req, res, next) {
+    res.render('rejoice', { title: 'Express' });
+});
+
+router.get('/ruth', function(req, res, next) {
+    res.render('ruth', { title: 'Express' });
+});
+
+router.get('/silfat', function(req, res, next) {
+    res.render('silfat', { title: 'Express' });
+});
+
+router.get('/yusuf', function(req, res, next) {
+    res.render('yusuf', { title: 'Express' });
+});
+
+router.get('/isaac', function(req, res, next) {
+    res.render('isaac', { title: 'Express' });
+});
+
+router.get('/betty', function(req, res, next) {
+    res.render('betty', { title: 'Express' });
+});
+
+
+
+
+router.get('/adedeji', function(req, res, next) {
+    res.render('adedeji', { title: 'Express' });
+
+});router.get('/anakam', function(req, res, next) {
+    res.render('anakam', { title: 'Express' });
+
+});router.get('/benjamin', function(req, res, next) {
+    res.render('benjamin', { title: 'Express' });
+
+});router.get('/bolaji', function(req, res, next) {
+    res.render('bolaji', { title: 'Express' });
+
+});router.get('/carol', function(req, res, next) {
+    res.render('carol', { title: 'Express' });
+
+});router.get('/chioma', function(req, res, next) {
+    res.render('chioma', { title: 'Express' });
+
+});router.get('/christiana', function(req, res, next) {
+    res.render('christiana', { title: 'Express' });
+
+});router.get('/dahiru', function(req, res, next) {
+    res.render('dahiru', { title: 'Express' });
+
+});router.get('/eric', function(req, res, next) {
+    res.render('eric', { title: 'Express' });
+
+});router.get('/ezra', function(req, res, next) {
+    res.render('ezra', { title: 'Express' });
+
+});router.get('/faith', function(req, res, next) {
+    res.render('faith', { title: 'Express' });
+
+});router.get('/gideon', function(req, res, next) {
+    res.render('gideon', { title: 'Express' });
+
+});router.get('/hr', function(req, res, next) {
+    res.render('hr', { title: 'Express' });
+
+});router.get('/idakpo', function(req, res, next) {
+    res.render('idakpo', { title: 'Express' });
+
+});router.get('/ifeanyi', function(req, res, next) {
+    res.render('ifeanyi', { title: 'Express' });
+
+});router.get('/jibo', function(req, res, next) {
+    res.render('jibo', { title: 'Express' });
+
+});router.get('/khalid', function(req, res, next) {
+    res.render('khalid', { title: 'Express' });
+
+});router.get('/lizzy', function(req, res, next) {
+    res.render('lizzy', { title: 'Express' });
+
+});router.get('/mary', function(req, res, next) {
+    res.render('mary', { title: 'Express' });
+
+});router.get('/nafiu', function(req, res, next) {
+    res.render('nafiu', { title: 'Express' });
+
+});router.get('/nwachukwu', function(req, res, next) {
+    res.render('nwachukwu', { title: 'Express' });
+
+});router.get('/obochi', function(req, res, next) {
+    res.render('obochi', { title: 'Express' });
+
+});router.get('/ochuwa', function(req, res, next) {
+    res.render('ochuwa', { title: 'Express' });
+
+});router.get('/odeh', function(req, res, next) {
+    res.render('odeh', { title: 'Express' });
+
+});router.get('/patience', function(req, res, next) {
+    res.render('patience', { title: 'Express' });
+
+});router.get('/samuel', function(req, res, next) {
+    res.render('samuel', { title: 'Express' });
+
+});router.get('/solomon', function(req, res, next) {
+    res.render('solomon', { title: 'Express' });
+
+});router.get('/tahiru', function(req, res, next) {
+    res.render('tahiru', { title: 'Express' });
+
+});router.get('/theodora', function(req, res, next) {
+    res.render('theodora', { title: 'Express' });
+
+});router.get('/tope', function(req, res, next) {
+    res.render('tope', { title: 'Express' });
+
+});router.get('/wumi', function(req, res, next) {
+    res.render('wumi', { title: 'Express' });
+
+});router.get('/jessica', function(req, res, next) {
+    res.render('jessica', { title: 'Express' });
+
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
